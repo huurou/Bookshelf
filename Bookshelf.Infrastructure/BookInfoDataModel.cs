25af43aae591225d81453ab00f2954ddb89f9c7c
kitada <taihei.kitada@gmail.com>
2023-03-05 22:05:04 +0900

    #4 BookInfoFetcherGoogleBooks実装

---
+++ Bookshelf.Infrastructure/BookInfoDataModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookshelf.Infrastructure;
+
+/// <summary>
+/// 本情報のデータモデル
+/// </summary>
+/// <param name="Id">ID</param>
+/// <param name="Title">タイトル</param>
+/// <param name="Author">著者</param>
+/// <param name="ImageStream">表紙画像ストリーム 大きい画像</param>
+/// <param name="ThumbnailStream">表紙サムネイルストリーム 小さい画像</param>
+/// <param name="PublishDate">発売日</param>
+/// <param name="Publisher">出版社</param>
+public record BookInfoDataModel(
+    string Id,
+    string Title,
+    string Author,
+    Stream? ImageStream,
+    Stream? ThumbnailStream,
+    string PublishDate,
+    string Publisher);
