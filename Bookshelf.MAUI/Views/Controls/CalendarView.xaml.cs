a9d2d45c50fbc510c9ec7c1190798f900e6c9025
ronron1114 <ronron1114>
2023-03-05 17:31:51 +0900

    月の切り替え

--- Bookshelf.MAUI/Views/Controls/CalendarView.xaml.cs
+++ Bookshelf.MAUI/Views/Controls/CalendarView.xaml.cs
@@ -4,30 +4,47 @@ namespace Bookshelf.MAUI.Views.Controls;
 
 public partial class CalendarView : ContentView
 {
+    public static readonly BindableProperty YearMonthProperty = BindableProperty.Create("YearMonth", typeof(DateTime), typeof(CalendarCellView), DateTime.Today);
+
+    public DateTime YearMonth
+    {
+        get => (DateTime)GetValue(YearMonthProperty);
+        set
+        {
+            SetValue(YearMonthProperty, value);
+
+            var ym = new DateTime(value.Year, value.Month, 1);
+            YearMonthLabel.Text = ym.ToString("yyyyîNMMåé");
+
+            //èâä˙âª
+            foreach (var child in CalendarGrid.Children)
+            {
+                ((CalendarCellView)child).DayLabel = "";
+                ((CalendarCellView)child).BackgroundColor = Colors.AliceBlue;
+            }
+
+            var lastDay = ym.AddMonths(1).AddDays(-1).Day;
+            var day = 1;
+            foreach (var child in CalendarGrid.Children.Skip((int)ym.DayOfWeek).Take(lastDay))
+            {
+                _dayCellDic[day] = (CalendarCellView)child;
+                ((CalendarCellView)child).DayLabel = day.ToString();
+                day++;
+            }
+
+            var today = DateTime.Today;
+            if (ym.Year == today.Year && ym.Month == today.Month)
+            {
+                _dayCellDic[today.Day].BackgroundColor = Colors.LightCoral;
+            }
+        }
+    }
+
     private DateTime _ym = new();
     private Dictionary<int, CalendarCellView> _dayCellDic = new();
 
     public CalendarView()
 	{
 		InitializeComponent();
-
-        _ym = new DateTime(2023, 3, 1);
-
-
-        var lastDay = _ym.AddMonths(1).AddDays(-1).Day;
-        var day = 1;
-        foreach (var child in CalendarGrid.Children.Skip((int)_ym.DayOfWeek).Take(lastDay))
-        {
-            //_dayCellDic.Add(day, child);
-            _dayCellDic[day] = (CalendarCellView)child;
-            ((CalendarCellView)child).DayLabel = day.ToString();
-            day++;
-        }
-
-        var today = DateTime.Today;
-        if (_ym.Year == today.Year && _ym.Month == today.Month)
-        {
-            _dayCellDic[today.Day].BackgroundColor = Colors.LightCoral;
-        }
     }
 }
--- Bookshelf.MAUI/Views/MainPage.xaml.cs
+++ Bookshelf.MAUI/Views/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Windows.Storage.Streams;
+
 namespace Bookshelf.MAUI.Views;
 
 public partial class MainPage : ContentPage
@@ -5,5 +7,24 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        _ym = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+        Calendar.YearMonth = _ym;
+
+        image.Source = "";
+    }
+
+    private DateTime _ym = new();
+
+    private void L_Button_Clicked(object sender, EventArgs e)
+    {
+        _ym = _ym.AddMonths(-1);
+        Calendar.YearMonth = _ym;
+    }
+
+    private void R_Button_Clicked(object sender, EventArgs e)
+    {
+        _ym = _ym.AddMonths(1);
+        Calendar.YearMonth = _ym;
+    }
 }
